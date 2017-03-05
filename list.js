ff28eba7eb14e01d0d29b8815dc1d4c772a85bf4
Gaurav Nanda <93.gaurav@gmail.com>
2017-03-05 20:24:47 +0530

    Initial Commit

---
+++ list.js
@@ -0,0 +1,52 @@
+class List {
+    constructor() {
+        this.memory = [];
+        this.length = 0;
+    }
+
+    get(address) {
+        return this.memory[address];
+    }
+
+    push(value) {
+        this.memory[this.length] = value;
+        this.length++;
+    }
+
+    pop() {
+        if (this.length === 0) {
+            return;
+        }
+
+        let currentIndex = this.length - 1;
+        let value = this.memory[currentIndex];
+        delete this.memory[this.length - 1];
+
+        this.length--;
+
+        return value;
+    }
+
+    unshift(value) {
+        for (let i = this.length; i >= 1; i--) {
+            this.memory[i] = this.memory[i - 1];
+        }
+
+        this.memory[0] = value;
+        this.length++;
+    }
+
+    shift() {
+        let value = this.memory[0];
+
+        for (let i = 1; i < this.length; i++) {
+            this.memory[i - 1] = this.memory[i];
+        }
+
+
+        delete this.memory[this.length - 1];
+        this.length--;
+
+        return value;
+    }
+}
